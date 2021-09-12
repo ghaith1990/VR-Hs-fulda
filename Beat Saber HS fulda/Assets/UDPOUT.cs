24de00448951fedeac82ea621c29d27b188a0599
ghaitho0o <38537334+ghaith1990@users.noreply.github.com>
2021-09-12 23:10:21 +0200

    startup script

--- Beat Saber HS fulda/Assets/UDPOUT.cs
+++ Beat Saber HS fulda/Assets/UDPOUT.cs
@@ -1,16 +1,45 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
+using System.Net.Sockets;
+using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UDPOUT : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class UdpOUT : MonoBehaviour
+
+{
+
+    private UdpClient maxClient = new UdpClient();
+    private ASCIIEncoding byteEncoder = new ASCIIEncoding();
+
+    void Start()
+    {
+        // Adress and Port
+        maxClient.Connect("127.0.0.1", 7470);
+    }
+
+
+    // sends an int value to Max
+
+    public void Sendint(string routeString, int val)
+    {
+        string message = routeString + ' ' + val;
+        byte[] messageBytes = byteEncoder.GetBytes(message);
+        SendBytes(messageBytes);
+    }
+
+    private void SendBytes(byte[] message)
+    {
+        try
+        {
+            maxClient.Send(message, message.Length);
+        }
+        catch (SocketException e)
+        {
+            Debug.Log("Max does not respond" + e);
+        }
+    }
 }
+
+
